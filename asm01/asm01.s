22969d9cae795f5bf2caadeda0b05fa0d623ed75
MehdiUFC <56354507+mehdi966@users.noreply.github.com>
2024-05-28 11:31:00 +0200

    Update asm01.s

--- asm01/asm01.s
+++ asm01/asm01.s
@@ -1,1 +1,20 @@
+global _start
 
+section .text
+    
+_start : 
+    mov rax, 1
+    mov rdi, 1 
+    mov rsi, msg
+    mov rdx, msglen
+
+    syscall
+
+    mov rax, 60 
+    xor rdi, rdi 
+
+    syscall
+
+section .data 
+    msg : db "1337"
+    msglen: equ $ - msg
