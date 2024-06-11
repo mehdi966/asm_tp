89c0922f2e530264421eedd591f4879d4eaef953
MehdiUFC <56354507+mehdi966@users.noreply.github.com>
2024-06-11 18:00:41 +0200

    Update asm03.s

--- asm03/asm03.s
+++ asm03/asm03.s
@@ -1,19 +1,22 @@
 section .data
     msg db "1337", 0
 
+section .bss
+    num resb 8
+
 section .text
     global asm03
 
 asm03:
     ; Vérifier le nombre d'arguments
-    mov rdi, [rsp]
-    cmp rdi, 2
+    mov rax, [rsp]   ; Nombre d'arguments
+    cmp rax, 2
     jne return_1
 
     ; Comparer l'argument avec 42
-    mov rdi, [rsp+8]
-    mov rsi, [rdi]
-    cmp rsi, 42
+    mov rdi, [rsp+8] ; Adresse de la chaîne de l'argument
+    call string_to_int
+    cmp rax, 42
     je display_and_return_0
 
 return_1:
@@ -28,4 +31,21 @@ display_and_return_0:
     syscall
 
     mov eax, 0
+    ret
+
+string_to_int:
+    ; Convertir la chaîne en entier
+    xor rax, rax    ; Clear RAX (result)
+    xor rcx, rcx    ; Clear RCX (multiplier)
+    xor rdx, rdx    ; Clear RDX (digit)
+convert_loop:
+    mov dl, byte [rdi + rcx]
+    test dl, dl
+    je end_convert
+    sub dl, '0'
+    imul rax, rax, 10
+    add rax, rdx
+    inc rcx
+    jmp convert_loop
+end_convert:
     ret
