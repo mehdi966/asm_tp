d7db8887344feffe670c1829a057e12fc5af276c
MehdiUFC <56354507+mehdi966@users.noreply.github.com>
2025-01-15 17:45:11 +0100

    Update asm02.s

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,68 +1,70 @@
 global _start
+section .bss
+    input resb 2
 
 section .data
-    msg db "rentrer un entier :",10
-    msglen equ $ - msg
+    msg: db "1337", 01
+    .len: equ $ - msg
+    help: db "Ask user to input a number, return 1337 if input is 42", 10
+    .lenHelp: equ $ - help
+    usage: db "USAGE : ./asm02", 10
+    .lenUsage: equ $ - usage
 
-    successMsg db "1337", 10
-    successMsgLen equ $ - successMsg
-    
 
-section .bss
-    userinput resb 32
 
 section .text
-
-_start : 
-
-    mov rax, 1
-    mov rdi, 1 
-    mov rsi, msg
-    mov rdx, msglen
-
-    syscall
-
-
-_stock : 
-
-    mov rax, 0 
+_start:
+    
+    mov rax, 0
     mov rdi, 0
-    mov rsi, userinput
-    mov rdx, 32
+    mov rsi, input
+    mov rdx, 4
+    syscall         ; we ask for user input
 
-    syscall
-
-_write : 
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, successMsg
-    mov rdx, successMsgLen
-
-    syscall
-
-_cmp : 
-    mov al, [userinput]
+    mov al, [input] ; we check if the first char is 4
     cmp al, '4'
-    jne _fail
-    mov al, [userinput+1]
+    jne _not42
+
+    mov al, [input + 1] ; we check if the second is 2
     cmp al, '2'
-    jne _fail
-    mov al, [userinput+2]
+    jne _not42
+
+    mov al, [input + 2] ; we check if the last is the newline char
     cmp al, 10
-    jne _fail
+    jne _not42
 
-_success:
-
-    mov rax, 60 
-    mov rdi, 0
-    
-    jmp _quit
-
-_fail : 
-    
-    mov rax, 60 
+_exit:
+    mov rax, 1        ; if the input is 42, we continue here and print 1337 exiting 0
     mov rdi, 1
-
-_quit :
-
+    mov rsi, msg
+    mov rdx, msg.len
     syscall
+ 
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+_not42:           ; if its not 42 we end here and exit 1 with no print
+    mov rax, 60
+    mov rdi, 1 
+    syscall
+
+_error:
+
+    mov rax, 1    ; print help message if there is an error
+    mov rdi, 1
+    mov rsi, help
+    mov rdx, help.lenHelp
+    syscall
+    
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, usage
+    mov rdx, usage.lenUsage
+    syscall
+
+
+    mov rax, 60
+    mov rdi, 1
+    syscall
+
