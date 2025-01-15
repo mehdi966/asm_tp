9a6e40a35fef0ff68745dc25a4184716fb648ec5
MehdiUFC <56354507+mehdi966@users.noreply.github.com>
2025-01-15 17:44:44 +0100

    Update asm01.s

--- asm01/asm01.s
+++ asm01/asm01.s
@@ -1,20 +1,19 @@
-global _start
+msg: db "1337", 01
+.len: equ $ - msg
+
+; SIMPLE CODE THAT PRINTS 1337
 
 section .text
-    
-_start : 
+
+    global _start
+
+_start:
     mov rax, 1
-    mov rdi, 1 
+    mov rdi, 1
     mov rsi, msg
-    mov rdx, msglen
-
+    mov rdx, msg.len 
     syscall
 
-    mov rax, 60 
-    xor rdi, rdi 
-
+    mov rax, 60
+    mov rdi, 0
     syscall
-
-section .data 
-    msg : db "1337"
-    msglen: equ $ - msg
